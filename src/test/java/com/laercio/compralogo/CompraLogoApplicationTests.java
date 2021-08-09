59426b86f9e555c4da9938d21e8a916cfb9f37eb
naatsuh <ks.full.ks@gmail.com>
2021-08-08 23:05:45 -0300

    .

---
+++ src/test/java/com/laercio/compralogo/CompraLogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laercio.compralogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompraLogoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
